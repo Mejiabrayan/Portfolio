802d161ad7cde9f6cdbe31027831fa3463da59b6
mejiabrayan <learnwithbrayan@gmail.com>
2022-12-17 07:53:04 -0800

    updated folder structure, color scheme and remove unused img

--- app.js
+++
@@ -1,6 +0,0 @@
-// make h1 element fade in when page loads
-
-$(document).ready(function() {
-    $('h1').fadeIn(2000);
-    }
-);
---
+++ index.js
@@ -0,0 +1,2 @@
+// Will incorporate the React library and the ReactDOM library into the project 
+// and store them in the variables React and ReactDOM in the future
