d6fe3f60fa79d165e58a921cce7daa5ca2bb840f
mejiabrayan <learnwithbrayan@gmail.com>
2022-12-17 08:30:18 -0800

    updated project btn

---
+++ js/app.js
@@ -0,0 +1,14 @@
+
+// burger menu in vanilla js
+
+const burger = document.querySelector('.burger');
+
+const nav = document.querySelector('#'+burger.dataset.target);
+
+burger.addEventListener('click', () => {
+
+burger.classList.toggle('is-active');
+
+nav.classList.toggle('is-active');
+
+});
